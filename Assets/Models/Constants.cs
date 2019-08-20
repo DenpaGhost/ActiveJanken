3a0943b1d98dcb1bb1271d38ce3d034ef31adedc
Mitsuki Kurashige <az.kinosaki@gmail.com>
2019-08-20 21:26:52 +0900

    日本語化

--- Assets/Models/Constants.cs
+++ Assets/Models/Constants.cs
@@ -2,11 +2,11 @@ namespace Models
 {
     public static class Constants
     {
-        public const string WaitingMessage = "- Press any button to start battle -";
+        public const string WaitingMessage = "- ボタンを押して準備完了 -";
         public const string ReadyMessage = "- Ready... -";
         public const long EnablePickupSukumiTime = 1000;
         public const string WinningMessage = "Win!!";
         public const string LoseMessage = "Lose...";
-        public const string TapToReturn = "- Tap to return -";
+        public const string TapToReturn = "- ボタンを押して戻る -";
     }
 }
